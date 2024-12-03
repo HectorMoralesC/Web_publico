d4ffaea8a4cd208147e3de04c6aa19c314dea3f7
HectorMoralesC <125382174+HectorMoralesC@users.noreply.github.com>
2024-12-03 16:42:50 +0100

    CSS inicio sesión

--- user_dashboard.php
+++ user_dashboard.php
@@ -29,6 +29,7 @@ if (!$user) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Usuario - VirtDesk</title>
+    <link rel="stylesheet" href="css/user_dashboard.css">
 </head>
 <body>
     <h1>Bienvenido, <?= htmlspecialchars($user['email']) ?></h1>
